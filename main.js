9e5d1f2d07512b77f26af378092f51d9036fe2e5
Erich Lersch <erich.lersch@immutable.com>
2022-07-19 16:01:38 -0300

    moving roads relative to the car

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const canvas = document.getElementById('myCanvas');
 canvas.width = 300;
 
 const ctx = canvas.getContext('2d');
-const road = new Road(canvas.width / 2, canvas.width * 0.9, 4);
-const car = new Car(road.getLaneCenter(6), 100, 30, 50);
+const road = new Road(canvas.width / 2, canvas.width * 0.9, 3);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 car.draw(ctx);
 
 animate();
@@ -12,7 +12,11 @@ animate();
 function animate() {
   car.update();
   canvas.height = window.innerHeight;
+
+  ctx.save();
+  ctx.translate(0, -car.y + canvas.height * 0.8);
   road.draw(ctx);
   car.draw(ctx);
+  ctx.restore();
   requestAnimationFrame(animate);
 }
--- road.js
+++ road.js
@@ -10,6 +10,16 @@ class Road {
     const fakeInfinity = 1000000;
     this.top = -fakeInfinity;
     this.bottom = fakeInfinity;
+
+    this.topLeft = { x: this.left, y: this.top };
+    this.bottomLeft = { x: this.left, y: this.bottom };
+    this.topRight = { x: this.right, y: this.top };
+    this.bottomRight = { x: this.right, y: this.bottom };
+
+    this.borders = [
+      [this.topLeft, this.bottomLeft],
+      [this.topRight, this.bottomRight],
+    ];
   }
 
   getLaneCenter(laneIndex) {
@@ -23,17 +33,20 @@ class Road {
     ctx.lineWidth = 5;
     ctx.strokeStyle = 'white';
 
-    for (let i = 0; i <= this.laneCount; i++) {
+    for (let i = 1; i <= this.laneCount - 1; i++) {
       const x = lerp(this.left, this.right, i / this.laneCount);
-      if (i > 0 && i < this.laneCount) {
-        ctx.setLineDash([20, 20]);
-      } else {
-        ctx.setLineDash([]);
-      }
+      ctx.setLineDash([20, 20]);
       ctx.beginPath();
       ctx.moveTo(x, this.top);
       ctx.lineTo(x, this.bottom);
       ctx.stroke();
     }
+    ctx.setLineDash([]);
+    this.borders.forEach((border) => {
+      ctx.beginPath();
+      ctx.moveTo(border[0].x, border[0].y);
+      ctx.lineTo(border[1].x, border[1].y);
+      ctx.stroke();
+    });
   }
 }
