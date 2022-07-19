d075391bfd0c5b86b21737e1105883ac49f4c6ba
Erich Lersch <erich.lersch@immutable.com>
2022-07-19 18:11:16 -0300

    car sensor pt 2

--- sensor.js
+++ sensor.js
@@ -1,21 +1,57 @@
 class Sensor {
-  constructor(car, rayCount = 3, rayLength = 100) {
+  constructor(car, rayCount = 10, rayLength = 150) {
     this.car = car;
     this.rayCount = rayCount;
     this.rayLength = rayLength;
-    this.raySpread = Math.PI / 4;
+    this.raySpread = Math.PI / 2;
 
     this.rays = [];
+    this.readings = [];
   }
 
-  update() {
+  update(roadBorders) {
+    this.#castRays();
+    this.readings = [];
+    for (let i = 0; i < this.rays.length; i++) {
+      this.readings.push(this.#getReading(this.rays[i], roadBorders));
+    }
+  }
+
+  draw(ctx) {
+    for (let i = 0; i < this.rayCount; i++) {
+      let color = 'yellow';
+      if (this.rays[i] !== undefined) {
+        let end = this.rays[i][1];
+        if (this.readings[i] !== undefined) {
+          end = this.readings[i];
+          color = 'red';
+        }
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.lineWidth = 2;
+        ctx.moveTo(this.rays[i][0].x, this.rays[i][0].y);
+        ctx.lineTo(end.x, end.y);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = 2;
+        ctx.moveTo(end.x, end.y);
+        ctx.lineTo(this.rays[i][1].x, this.rays[i][1].y);
+        ctx.stroke();
+      }
+    }
+  }
+
+  #castRays() {
     this.rays = [];
     for (let i = 0; i < this.rayCount; i++) {
-      const angle = lerp(
-        this.raySpread / 2,
-        -this.raySpread / 2,
-        i / (this.rayCount - 1)
-      );
+      const angle =
+        lerp(
+          this.raySpread / 2,
+          -this.raySpread / 2,
+          this.rayCount == 1 ? 0.5 : i / (this.rayCount - 1)
+        ) + this.car.angle;
       const start = {
         x: this.car.x,
         y: this.car.y,
@@ -26,19 +62,26 @@ class Sensor {
       };
       this.rays.push([start, end]);
     }
-    console.log(this.rays);
   }
 
-  draw(ctx) {
-    for (let i = 0; i < this.rayCount; i++) {
-      if (this.rays[i] !== undefined) {
-        ctx.beginPath();
-        ctx.lineWidth = 2;
-        ctx.strokeStyle = 'yellow';
-        ctx.moveTo(this.rays[i][0].x, this.rays[i][0].y);
-        ctx.lineTo(this.rays[i][1].x, this.rays[i][1].y);
-        ctx.stroke();
+  #getReading(ray, roadBorders) {
+    let touches = [];
+    for (let i = 0; i < roadBorders.length; i++) {
+      const touch = getIntersection(
+        ray[0],
+        ray[1],
+        roadBorders[i][0],
+        roadBorders[i][1]
+      );
+      if (touch !== undefined) {
+        touches.push(touch);
       }
+      if (touches.length === 0) {
+        return null;
+      }
+      // order by the offset, get the smallest one (closest to the car)
+      // and return it
+      return touches.sort((a, b) => a.offset - b.offset)[0];
     }
   }
 }
