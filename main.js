881aaec6aad538102fc57f227f54fa346a4c1474
Erich Lersch <erich.lersch@immutable.com>
2022-07-20 17:39:23 -0300

    can save the best of the pack

--- main.js
+++ main.js
@@ -9,8 +9,20 @@ const networkCtx = networkCanvas.getContext('2d');
 
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9, 3);
 const cars = generateCars(100);
+let bestCar = cars[0];
+if (localStorage.getItem('bestBrain')) {
+  bestCar.brain = JSON.parse(localStorage.getItem('bestBrain'));
+}
 const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, 'DUMMY', 2)];
 animate();
+
+function save() {
+  localStorage.setItem('bestBrain', JSON.stringify(bestCar.brain));
+}
+
+function discard() {
+  localStorage.removeItem('bestBrain');
+}
 
 function generateCars(N) {
   const cars = [];
@@ -29,7 +41,7 @@ function animate(time) {
     cars[i].update(road.borders, traffic);
   }
 
-  const bestCar = cars.find((c) => c.y == Math.min(...cars.map((mc) => mc.y)));
+  bestCar = cars.find((c) => c.y == Math.min(...cars.map((mc) => mc.y)));
 
   carCanvas.height = window.innerHeight;
   networkCanvas.height = window.innerHeight;
